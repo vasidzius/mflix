11a97ad04fd9881e9ff916cf929c160f3338690f
Vasiliy.Kovalchenko <Vasiliy.Kovalchenko@extraenergy.com.cy>
2018-12-18 11:59:19 +0200

    first commit

---
+++ mongodb-analytics/mflix/src/main/java/com/vasidzius/learnmongo/mflix/MflixApplication.java
@@ -0,0 +1,14 @@
+package com.vasidzius.learnmongo.mflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MflixApplication.class, args);
+	}
+
+}
+
---
+++ mongodb-analytics/mflix/src/test/java/com/vasidzius/learnmongo/mflix/MflixApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vasidzius.learnmongo.mflix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MflixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
